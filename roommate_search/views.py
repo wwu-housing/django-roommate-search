4334ac211053133fca55eab5dfa2578616bb5a74
Firass Asad <Firass.Asad@wwu.edu>
2012-01-13 03:57:03 -0800

    replace profile view with an object that gets a user's profile that is extended by create, update, and detail views

--- roommate_search/views.py
+++ roommate_search/views.py
@@ -1,38 +1,65 @@
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
-from django.views.generic import TemplateView
-from django.views.generic.detail import SingleObjectTemplateResponseMixin
-from django.views.generic.edit import ModelFormMixin, ProcessFormView
+from django.views.generic import (CreateView, DetailView, TemplateView,
+                                  UpdateView)
 
 from forms import ProfileForm
 from models import Profile
 
 
-class ProfileView(SingleObjectTemplateResponseMixin, ModelFormMixin, ProcessFormView):
-    """roommate search profile view"""
-    template_name = "roommate_search/profile_detail.html"
-    model = Profile
-
-    def get_object(self, request):
-        try:
-            return Profile.objects.get(user=request.user)
-        except Profile.DoesNotExist:
-            return None
+class GetProfileObject(object):
+    def get_object(self):
+        return Profile.objects.get(user=self.request.user)
 
     def get_form_class(self):
         return ProfileForm
 
-    def get(self, request, *args, **kwargs):
-        self.object = self.get_object(request)
-        return super(ProfileView, self).get(request, *args, **kwargs)
+    def get_success_url(self):
+        return reverse("roommate_search_profile")
 
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object(request)
-        return super(ProfileView, self).post(request, *args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        try:
+            self.object = self.get_object()
+        except Profile.DoesNotExist:
+            return redirect(reverse("roommate_search_add_profile"))
+
+        return super(GetProfileObject, self).get(request, *args, **kwargs)
 
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
-        return super(ProfileView, self).dispatch(request, *args, **kwargs)
+        return super(GetProfileObject, self).dispatch(request, *args, **kwargs)
+
+
+class ProfileCreateView(GetProfileObject, CreateView):
+    model = Profile
+
+    def get_object(self):
+        try:
+            return Profile.objects.get(user=self.request.user)
+        except Profile.DoesNotExist:
+            return None
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        return super(ProfileCreateView, self).form_valid(form)
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object:
+            return redirect(reverse("roommate_search_profile"))
+
+        return super(ProfileCreateView, self).get(request, *args, **kwargs)
+
+
+class ProfileDetailView(GetProfileObject, DetailView):
+    pass
+
+
+class ProfileUpdateView(GetProfileObject, UpdateView):
+    pass
 
 
 class SearchView(TemplateView):
