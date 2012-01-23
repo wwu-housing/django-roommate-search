bb77f9fe50a3d98e52d90dfa50c4ef9b189ec39b
Firass Asad <Firass.Asad@wwu.edu>
2012-01-23 12:21:36 -0800

    include urls for django-messages; add comments for groups of urls

--- roommate_search/urls.py
+++ roommate_search/urls.py
@@ -8,9 +8,15 @@ from views import (ProfileCreateView, ProfileDetailView, ProfileUpdateView,
 urlpatterns = patterns("",
     url(r"^$", TemplateView.as_view(template_name="roommate_search/index.html"),
         name="roommate_search_index"),
+
+    # profile
     url(r"^profile/$", ProfileDetailView.as_view(), name="roommate_search_profile"),
     url(r"^profile/add/$", ProfileCreateView.as_view(), name="roommate_search_add_profile"),
     url(r"^profile/edit/$", ProfileUpdateView.as_view(), name="roommate_search_edit_profile"),
+
+    # search
     url(r"^search/$", SearchView.as_view(), name="roommate_search_search"),
 
+    # django-messages
+    (r"^messages/", include("django_messages.urls")),
 )
