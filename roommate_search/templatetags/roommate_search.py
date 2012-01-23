53c742c1c6e835bbedf2f881f9355e4472fb379f
Firass Asad <Firass.Asad@wwu.edu>
2012-01-23 12:25:26 -0800

    add fitzgen's screenname template tag

---
+++ roommate_search/templatetags/roommate_search.py
@@ -0,0 +1,14 @@
+from ..models import Profile
+from django.template import Library
+
+
+register = Library()
+
+@register.filter
+def screen_name(user):
+    """
+    A filter for getting the screen name of a user in a template.
+
+    {{ request.user|screen_name }}
+    """
+    return Profile.objects.get(user=user).screen_name
