ccca8bbf86adf563ca7486109761f00a626df733
Firass Asad <Firass.Asad@wwu.edu>
2012-01-24 10:35:07 -0800

    add limit fields to display in the profile form

--- roommate_search/forms.py
+++ roommate_search/forms.py
@@ -6,6 +6,7 @@ from models import Profile
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
+        fields = ("screen_name", "bio", "status")
         widgets = {
             "status": forms.RadioSelect(),
         }
