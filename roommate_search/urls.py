f64f0bc07cfc3e7bcc0ddd1abc78918e21f70065
Firass Asad <Firass.Asad@wwu.edu>
2012-01-25 16:08:19 -0800

    add url mapping to public profile detail view

--- roommate_search/urls.py
+++ roommate_search/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic import DetailView, TemplateView
+from django.views.generic import TemplateView
 
 from views import (ProfileCreateView, ProfileDetailView, ProfileUpdateView,
-                   SearchView, StarsView)
+                   PublicProfileDetailView, SearchView, StarsView)
 from models import Profile
 
 
@@ -18,7 +18,7 @@ urlpatterns = patterns("",
     url(r"^profile/$", ProfileDetailView.as_view(), name="roommate_search_profile"),
 
     # TODO: make PublicProfileDetailView to limit a user to viewing search set profiles.
-    url(r"^profile/(?P<pk>\d+)/$", DetailView.as_view(model=Profile), name="roommate_search_public_profile"),
+    url(r"^profile/(?P<pk>\d+)/$", PublicProfileDetailView.as_view(), name="roommate_search_public_profile"),
     url(r"^profile/add/$", ProfileCreateView.as_view(), name="roommate_search_add_profile"),
     url(r"^profile/edit/$", ProfileUpdateView.as_view(), name="roommate_search_edit_profile"),
 
