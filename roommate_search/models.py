2fae7e80a49d7a2034e47ce298c0eea6754d2951
Firass Asad <Firass.Asad@wwu.edu>
2012-05-10 14:33:15 -0700

    change profile to make user a one-to-one field

--- roommate_search/models.py
+++ roommate_search/models.py
@@ -24,7 +24,7 @@ class Profile(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default="looking")
-    user = models.ForeignKey(User, related_name="roommate_search_profiles")
+    user = models.OneToOneField(User)
     clusters = models.ManyToManyField(Cluster, related_name="profiles")
     stars = models.ManyToManyField("self",
                                    symmetrical=False,
